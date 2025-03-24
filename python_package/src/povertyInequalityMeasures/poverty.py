b996f5a8d7e57dcc249b9c72254108ac639ed5f0
Daniel Mermelstein <danmermel@gmail.com>
2025-03-24 10:51:03 +0000

    In this iteration I added the concept of target_col and weight_col target_col is the column that will be used to measure poverty.. this could be income or expenditure or somethhing else weight_col is the number of actual people each hh row represents I also updated the test to reflect that

--- python_package/src/povertyInequalityMeasures/poverty.py
+++ python_package/src/povertyInequalityMeasures/poverty.py
@@ -1,8 +1,14 @@
-def get_headcount_index(pl,data):
+def get_headcount_index(pl,data,target_col,weight_col):
     #pl is the poverty line
     #data is a dataframe containing survey data
-    total_sample = data.shape[0] # number of rows
-    the_poor = data[data['total_expenditure'] < pl].shape[0]
+    #target_col is the column within the data that will be used. It can be expenditure, income, or something else
+    #weight_col is the column that has the weight of each row (i.e. the number of actual households that a row represents)
+    total_sample = data[weight_col].sum() # the number of actual people each hh row represents
+    #print(total_sample)
+    the_poor = data[data[target_col] < pl]  # a dataframe that only includes poor people, ie those below the poverty line
     #print("the poor are ", the_poor)
-    poverty_index = the_poor / total_sample
+    the_weighted_poor = the_poor[weight_col].sum()  # the actual amount of poor people based on the number of people each household represents
+    #print(the_weighted_poor)
+    poverty_index = the_weighted_poor / total_sample
     return poverty_index
+
--- python_package/tests/test.py
+++ python_package/tests/test.py
@@ -9,9 +9,9 @@ class TestPovertyMeasures(unittest.TestCase):
         """
         Test the headline poverty function
         """
-        data = pd.DataFrame({'total_expenditure': [ 110,120,150,160]})
+        data = pd.DataFrame({'total_expenditure': [ 110,120,150,160], 'weight':[1,1,1,1]})
         poverty_line= 125
-        result = poverty.get_headcount_index(poverty_line, data)
+        result = poverty.get_headcount_index(poverty_line, data, "total_expenditure","weight")
         self.assertEqual(result, 0.5)
 
 if __name__ == '__main__':
