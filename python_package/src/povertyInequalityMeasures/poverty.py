e6e8dce836e870c3856d81d8abe3608d0b1c714e
Daniel Mermelstein <danmermel@gmail.com>
2025-03-31 19:05:21 +0100

    In this iteration I added the Watts index and the time to exit function I also added a test for the watts index and fixed some tests (I given them all the same name and hence they were not running)
    Signed-off-by: Daniel Mermelstein <danmermel@gmail.com>

--- python_package/src/povertyInequalityMeasures/poverty.py
+++ python_package/src/povertyInequalityMeasures/poverty.py
@@ -1,3 +1,5 @@
+import math
+
 def get_headcount_index(pl,data,target_col,weight_col):
     #pl is the poverty line
     #data is a dataframe containing survey data
@@ -31,7 +33,7 @@ def get_poverty_severity_index(pl, data, target_col, weight_col):
     for i in range(0,total_sample):
         data.loc[i,"poverty_gap_squared"] = (max(0, (pl-data.loc[i,target_col])/pl))**2
     poverty_severity_index = (data["poverty_gap_squared"]*data[weight_col]).sum() / total_sample_weighted
-    print(data)
+    #print(data)
     return round(poverty_severity_index,5)
 
 def get_poverty_severity_index_generic(pl, data, target_col, weight_col,alpha):
@@ -50,15 +52,35 @@ def get_poverty_severity_index_generic(pl, data, target_col, weight_col,alpha):
             #below the poverty line 
             data.loc[i,"poverty_gap_alpha"] = ((pl-data.loc[i,target_col])/pl)**alpha
     poverty_severity_index_generic = (data["poverty_gap_alpha"]*data[weight_col]).sum() / total_sample_weighted
-    print(data)
+    #print(data)
     return round(poverty_severity_index_generic,5)
 
 def get_sen_index(pl,data, target_col, weight_col):
     pov_headcount = get_headcount_index(pl,data, target_col, weight_col)
-    print(pov_headcount)
+    #print(pov_headcount)
     pov_gap = get_poverty_gap_index(pl,data, target_col, weight_col)
-    print(pov_gap)
+    #print(pov_gap)
     #for now let's just make Ginni a constant until i figure out how to calculate it
     gini = 0.7
     sen_index = pov_headcount*gini + pov_gap*(1-gini)
     return sen_index
+
+def get_watts_index (pl, data,target_col, weight_col):
+    total_sample = data.shape[0] # number of rows
+    total_sample_weighted = data[weight_col].sum()  #number of actual people the sample represents
+    watts_total=0 # to contain our adding of the totals below
+    for i in range(0,total_sample):
+        if data.loc[i, target_col] < pl:
+            ## add it to the total according to the Watts formula, because the sum is over individuals whose income/expenditure falls below the pl
+            watts_total += (math.log(pl/data.loc[i,target_col]))*data.loc[i,weight_col]
+    #finally divide by the total sample
+    watts_total = watts_total / total_sample_weighted
+    return round(watts_total,5)
+
+def get_time_to_exit(pl,data, growth, target_col):
+    total_sample = data.shape[0] # number of rows
+    for i in range(0,total_sample):
+        if data.loc[i, target_col] < pl:
+            #add a time to exit to that row
+            data.loc[i, "time_to_exit"] = round((math.log(pl/data.loc[i,target_col]))/growth,2)
+    return data
--- python_package/tests/test.py
+++ python_package/tests/test.py
@@ -24,15 +24,15 @@ class TestPovertyMeasures(unittest.TestCase):
         self.assertEqual(result, 0.08)
     
     def test_poverty_severity(self):
-        """
-        Test the poverty severity index function
-        """
-        data = pd.DataFrame({'total_expenditure': [ 100,110,150,160], 'weight':[1,1,1,1]})
-        poverty_line= 125
-        result = poverty.get_poverty_severity_index(poverty_line, data, "total_expenditure","weight")
-        self.assertEqual(result, 0.0136)
+      """
+      Test the poverty severity index function
+      """
+      data = pd.DataFrame({'total_expenditure': [ 100,110,150,160], 'weight':[1,1,1,1]})
+      poverty_line= 125
+      result = poverty.get_poverty_severity_index(poverty_line, data, "total_expenditure","weight")
+      self.assertEqual(result, 0.0136)
 
-    def test_poverty_severity_generic(self):
+    def test_poverty_severity_generic_1(self):
         """
         Test the generic poverty severity index function
         """
@@ -41,7 +41,7 @@ class TestPovertyMeasures(unittest.TestCase):
         result = poverty.get_poverty_severity_index_generic(poverty_line, data, "total_expenditure","weight",0)
         self.assertEqual(result, 0.5)
 
-    def test_poverty_severity_generic(self):
+    def test_poverty_severity_generic_2(self):
         """
         Test the generic poverty severity index function
         """
@@ -50,7 +50,7 @@ class TestPovertyMeasures(unittest.TestCase):
         result = poverty.get_poverty_severity_index_generic(poverty_line, data, "total_expenditure","weight",1)
         self.assertEqual(result, 0.08)
 
-    def test_poverty_severity_generic(self):
+    def test_poverty_severity_generic_3(self):
         """
         Test the generic poverty severity index function
         """
@@ -59,14 +59,25 @@ class TestPovertyMeasures(unittest.TestCase):
         result = poverty.get_poverty_severity_index_generic(poverty_line, data, "total_expenditure","weight",2)
         self.assertEqual(result, 0.0136)
 
-def test_sen_index(self):
+    def test_sen_index(self):
         """
         Test the sen index function
         """
-        data = pd.DataFrame({'total_expenditure': [ 110,120,150,160], 'weight':[1,1,1,1]})
+        data = pd.DataFrame({'total_expenditure': [ 100,110,150,160], 'weight':[1,1,1,1]})
         poverty_line= 125
         result = poverty.get_sen_index(poverty_line, data, "total_expenditure","weight")
         self.assertEqual(result, 0.374)
+
+    def test_watts_index(self):
+        """
+        Test the watts index function
+        """
+        data = pd.DataFrame({'total_expenditure': [ 100,110,150,160], 'weight':[1,1,1,1]})
+        poverty_line= 125
+        result = poverty.get_watts_index(poverty_line, data, "total_expenditure","weight")
+        self.assertEqual(result, 0.08774)
+
+    # TODO def test_time_to_exit(self):
 
 if __name__ == '__main__':
     unittest.main()
