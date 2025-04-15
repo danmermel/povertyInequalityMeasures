44bf41be3653acc93e4c2f0832fe811425383fd5
Daniel Mermelstein <danmermel@gmail.com>
2025-04-15 21:29:56 +0100

    In this iteration I silenced a warning of a future deprecation that was coming out in the output FutureWarning: Calling float on a single element Series is deprecated and will raise a TypeError in the future. Use float(ser.iloc[0]) instead I was following this article: https://stackoverflow.com/questions/76256618/how-to-deal-with-futurewarning-regarding-applying-int-to-a-series-with-one-item#76848560 It works, but I don't understand why!
    Signed-off-by: Daniel Mermelstein <danmermel@gmail.com>

--- python_package/src/povertyInequalityMeasures/inequality.py
+++ python_package/src/povertyInequalityMeasures/inequality.py
@@ -28,7 +28,7 @@ def get_gini(data,target_col,weight_col):
             
     #print(total_area, lorenz_area)
     gini = (total_area-lorenz_area) / total_area
-    return float(round(gini,2))
+    return float(round(gini.iloc[0],2))
 
 def get_palma(data,target_col,weight_col):
     nrows = data.shape[0] # count of rows
@@ -60,7 +60,12 @@ def get_palma(data,target_col,weight_col):
     #print (accumulated_income_per_decile)
     target_per_decile = (sorted_data.groupby(['bins'],observed=False)['TARGET_WEIGHTED'].agg(['sum']))  
     #print(income_per_decile)
-    palma = float(target_per_decile.iloc[9]) / float(accumulated_target_per_decile.iloc[3])
-    #in other words the amount that the top decile has of the thing you are measuring divided by the amount that the bottom 4 deciles have
-    print(palma)
-    return float(round(palma,2))
+    print(type(target_per_decile))
+    palma = float(target_per_decile.iloc[9].iloc[0]) / float(accumulated_target_per_decile.iloc[3].iloc[0])
+    # in other words the amount that the top decile has of the thing you are measuring divided by the amount that the bottom 4 deciles have
+    return round(palma,2)
+    # also, not to future self: that strange double iloc notation was used to get rid of this warning:
+    # FutureWarning: Calling float on a single element Series is deprecated and will raise a TypeError in the future. Use float(ser.iloc[0]) instead
+    # was following this article: https://stackoverflow.com/questions/76256618/how-to-deal-with-futurewarning-regarding-applying-int-to-a-series-with-one-item#76848560
+    # it works, but I don't understand why!
+    
