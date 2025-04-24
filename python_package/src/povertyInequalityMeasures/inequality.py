28f4531c8eb7e619ac9fe36b24d4d1f3fb510527
Graham Stark <graham.stark@virtual-worlds.biz>
2025-04-24 22:31:45 +0100

    Update inequality.py
    more precision in inequality measures

--- python_package/src/povertyInequalityMeasures/inequality.py
+++ python_package/src/povertyInequalityMeasures/inequality.py
@@ -24,7 +24,7 @@ def get_gini(data,target_col,weight_col):
     lorenz_area /= (popn[-1] * target[-1])  # Normalize
     #print(lorenz_area)
     gini = 1 - lorenz_area
-    return round(gini,2)
+    return round(gini,5)
 
 def get_palma(data,target_col,weight_col):
     nrows = data.shape[0] # count of rows
@@ -49,7 +49,7 @@ def get_palma(data,target_col,weight_col):
     target_per_decile = (sorted_data.groupby(['bins'],observed=False)['TARGET_WEIGHTED'].agg(['sum']))  
     palma = float(target_per_decile.iloc[9].iloc[0]) / float(accumulated_target_per_decile.iloc[3].iloc[0])
     # in other words the amount that the top decile has of the thing you are measuring divided by the amount that the bottom 4 deciles have
-    return round(palma,2)
+    return round(palma,5)
     # also, not to future self: that strange double iloc notation was used to get rid of this warning:
     # FutureWarning: Calling float on a single element Series is deprecated and will raise a TypeError in the future. Use float(ser.iloc[0]) instead
     # was following this article: https://stackoverflow.com/questions/76256618/how-to-deal-with-futurewarning-regarding-applying-int-to-a-series-with-one-item#76848560
